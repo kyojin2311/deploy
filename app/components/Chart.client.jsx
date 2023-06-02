4b450a055e0a84a5e1ee0e0aba917e4e16239796
Pham Van Thach <21020239@vnu.edu.vn>
2023-06-02 12:14:02 +0700

    hehe

--- app/components/Chart.client.jsx
+++
@@ -1,7 +0,0 @@
-import Chart from "react-apexcharts";
-
-export default function ChartCom(props) {
-  return (
-    <Chart options={props.data.options} series={props.data.series} type="bar" />
-  );
-}
---
+++ app/components/Dashboard/Chart.client.jsx
@@ -0,0 +1,11 @@
+import Chart from "react-apexcharts";
+
+export default function ChartCom(props) {
+  return (
+    <Chart
+      options={props.data.options}
+      series={props.data.series}
+      type="area"
+    />
+  );
+}
